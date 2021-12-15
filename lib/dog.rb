208a0631b2cefa2af1e64aedd8dbfc92ee3618b7
Daniel Cheong <dcheo001@gmail.com>
2021-12-14 19:03:09 -0500

    commit tree 04bc06c9a0eb4c52b57dee8c74006247d7295a1c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(dog_name) #setting the dog's name
+        @this_dogs_name = dog_name
+    end
+  
+    def name #getting the dog's name
+        @this_dogs_name
+    end
+  end
 
-end
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
